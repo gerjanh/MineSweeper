c8609040b62691cfd6c3bda95c39aa014696de31
Gerjan <gerjanholsappel@hotmail.com>
2014-10-09 13:02:48 +0200

    added json

--- ConsoleApplication1/ConsoleApplication1/Server.cs
+++ ConsoleApplication1/ConsoleApplication1/Server.cs
@@ -3,10 +3,117 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MineSweeper.statics;
+using System.Net.Security;
+using System.Runtime.Remoting.Messaging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
 
 namespace MineSweeper
 {
     class Server
     {
+
+        private readonly byte[] Buffer = new byte[1024];
+        private const int _bufferSize = 1024;
+        private readonly TcpClient _tcpclient;
+        private readonly SslStream _sslStream;
+        private List<byte> _totalBuffer;
+
+
+        private void ThreadLoop()
+        {
+           /** while (true)
+            {
+                try
+                {
+                    //new Socket().Receive(Buffer);
+                   var receiveCount = _sslStream.Read(Buffer, 0, _bufferSize);
+
+                    byte[] rawData = new byte[receiveCount];
+                    Array.Copy(Buffer, 0, rawData, 0, receiveCount);
+                    _totalBuffer = _totalBuffer.Concat(rawData).ToList();
+
+
+                    int packetSize = Packet.getLengthOfPacket(_totalBuffer);
+                    if (packetSize == -1)
+                        continue;
+
+                    JObject json = Packet.RetrieveJSON(packetSize, ref _totalBuffer);
+
+                    if (json == null)
+                        continue;
+
+                    JToken cmd;
+                    if (!json.TryGetValue("CMD", out cmd))
+                    {
+                        Console.WriteLine("Got JSON that does not define a command.");
+                        continue;
+                    }
+
+                    var packetType = cmd.ToString().ToLower();
+
+                    switch (packetType)
+                    {
+                        case "login":
+                            HandleLoginPacket(json);
+                            break;
+                        default:
+                            Console.WriteLine("Unknown packet");
+                            break;
+                    }
+
+
+
+
+                    //_totalBuffer = _totalBuffer.Substring(packetSize + 4);
+                    //_totalBuffer = String.Empty;
+                }
+                catch (SocketException e)
+                {
+                    Console.WriteLine("Client with IP-address: " + _tcpclient.Client.LocalEndPoint + " has been disconnected.");
+                    Console.WriteLine(e.Message);
+                }
+            }*/
+        }
+
+        private void Send(String s)
+        {
+            //byte[] data = Encoding.UTF8.GetBytes(s.Length.ToString("0000") + s).ToArray();
+
+           // _sslStream.Write(Packet.CreateByteData(s));
+        }
+
+        private void HandleLoginPacket(JObject json)
+        {
+            //Recieve the username and password from json.
+            var username = json["USERNAME"].ToString();
+
+            JObject returnJson;
+            //Code to check user/pass here
+
+            returnJson =
+                   new JObject(
+                       new JProperty("CMD", "resp-login")//,
+                 //      new JProperty("status", new Statuscode("ok"));
+                    //   new JProperty("STATUS", Statuscode.GetCode(Statuscode.Status.InvalidUsernameOrPassword)),
+                    //   new JProperty("DESCRIPTION", Statuscode.GetDescription(Statuscode.Status.InvalidUsernameOrPassword))
+                       );
+
+            //Send the result back to the client.
+            Console.WriteLine(returnJson.ToString());
+            Send(returnJson.ToString());
+        }
+
+        public void setupfield(){
+
+        }
     }
 }
---
+++ ConsoleApplication1/ConsoleApplication1/statics/Statuscode.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineSweeper.statics
+{
+    class Statuscode
+    {
+    public class Statuscode
+    {
+        public int Code { get; private set; }
+        public string Description { get; private set; }
+
+        public enum Status
+        {
+            Ok = 200,
+            Unauthorized = 401,
+            AccessDenied = 403,
+            InvalidUsernameOrPassword = 430,
+            CommandNotFound = 500,
+            SyntaxError = 501,
+            CommandNotImplemented = 502
+        }
+        public Statuscode(Status status)
+        {
+            switch (status)
+            {
+                case Status.Ok:
+                    Code = 200;
+                    Description = "OK!";
+                    break;
+                case Status.Unauthorized:
+                    Code = 401;
+                    Description = "UNAUTHORIZED";
+                    break;
+                case Status.AccessDenied:
+                    Code = 403;
+                    Description = "ACCESS DENIED";
+                    break;
+                case Status.InvalidUsernameOrPassword:
+                    Code = 430;
+                    Description = "INVALID USERNAME OR PASSWORD";
+                    break;
+                case Status.CommandNotFound:
+                    Code = 500;
+                    Description = "COMMAND NOT FOUND";
+                    break;
+                case Status.SyntaxError:
+                    Code = 501;
+                    Description = "SYNTAX ERROR";
+                    break;
+                case Status.CommandNotImplemented:
+                    Code = 502;
+                    Description = "COMMAND NOT IMPLEMENTED";
+                    break;
+            }
+        }
+
+        public static int GetCode(Status status)
+        {
+            switch (status)
+            {
+                case Status.Ok:
+                    return 200;
+                case Status.Unauthorized:
+                    return 401;
+                case Status.AccessDenied:
+                    return 403;
+                case Status.InvalidUsernameOrPassword:
+                    return 430;
+                case Status.CommandNotFound:
+                    return 500;
+                case Status.SyntaxError:
+                    return 501;
+                case Status.CommandNotImplemented:
+                    return 502;
+            }
+            return 0;
+        }
+
+
+        public static string GetDescription(Status status)
+        {
+            switch (status)
+            {
+                case Status.Ok:
+                    return "OK!";
+                case Status.Unauthorized:
+                    return "UNAUTHORIZED";
+                case Status.AccessDenied:
+                    return "ACCESS DENIED";
+                case Status.InvalidUsernameOrPassword:
+                    return "INVALID USERNAME OR PASSWORD";
+                case Status.CommandNotFound:
+                    return "COMMAND NOT FOUND";
+                case Status.SyntaxError:
+                    return "SYNTAX ERROR";
+                case Status.CommandNotImplemented:
+                    return "COMMAND NOT IMPLEMENTED";
+            }
+            return String.Empty;
+        }
+
+    }
+}
+    }
+}
